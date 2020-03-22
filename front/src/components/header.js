ca487fccc44ae6cc3963ee7a192b73340788950c
JessieMTabilisma <jessiemtabilisma@gmail.com>
2020-03-22 18:53:15 +0800

    initial setup

---
+++ front/src/components/header.js
@@ -0,0 +1,31 @@
+import { Link } from 'gatsby'
+import PropTypes from 'prop-types'
+import React from 'react'
+
+const Header = ({ siteTitle }) => (
+  <header className="p-5 bg-black">
+    <div>
+      <h1 style={{ margin: 0 }}>
+        <Link
+          to="/"
+          style={{
+            color: `white`,
+            textDecoration: `none`,
+          }}
+        >
+          {siteTitle}
+        </Link>
+      </h1>
+    </div>
+  </header>
+)
+
+Header.propTypes = {
+  siteTitle: PropTypes.string,
+}
+
+Header.defaultProps = {
+  siteTitle: ``,
+}
+
+export default Header
